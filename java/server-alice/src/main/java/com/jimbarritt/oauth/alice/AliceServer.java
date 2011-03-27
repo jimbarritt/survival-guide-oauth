8c6c39c9ad3f56a98aab231c4d66b64ceac96dc2
Jim Barritt <jim@planet-ix.com>
2011-03-27 16:44:10 +0530

    [JimB] Adding server classes for each of the servers

---
+++ java/server-alice/src/main/java/com/jimbarritt/oauth/alice/AliceServer.java
@@ -0,0 +1,9 @@
+package java.com.jimbarritt.oauth.alice;
+
+public class AliceServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hey, perhaps I should start something up!");
+    }
+
+}
---
+++ java/server-bob/src/main/java/com/jimbarritt/oauth/bob/BobServer.java
@@ -0,0 +1,9 @@
+package com.jimbarritt.oauth.bob;
+
+public class BobServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hey perhaps I should put something in here!");
+    }
+
+}
